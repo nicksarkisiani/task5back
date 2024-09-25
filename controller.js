fb76d8428b099b7ea1d3b3a98dcf74bb13e24bb1
Haster1337 <hasterpro@gmail.com>
2024-09-25 23:42:06 +0400

    add error

--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
-
 const {Faker, ru, en_US, es, fakerRU, en} = require("@faker-js/faker")
+const {chance} = require("chance")
 
 /*
 
@@ -19,87 +19,188 @@ class Controller {
         this.generateUsID = this.generateUsID.bind(this);
         this.generateRuID = this.generateRuID.bind(this)
         this.generateEsID = this.generateEsID.bind(this);
+        this.deleteSymbolError = this.deleteSymbolError.bind(this);
+        this.changeIndexError = this.changeIndexError.bind(this);
+        this.addSymbolError = this.addSymbolError.bind(this);
+        this.oneErrorInData = this.oneErrorInData.bind(this);
     }
 
 
     async generateData(req, res) {
         try {
             const {locale, seed, errorCount, page} = req.body;
-            res.send(this.generateRandomUser(locale, seed))
+            let newSeed = seed + page
+            const users = []
+
+            const locales = {
+                en,
+                ru,
+                es
+            }
+            const faker = new Faker({locale: locales[locale]})
+            this.faker = faker
+            faker.seed(newSeed)
+            for(let i = 0; i < 10; i++) {
+
+                const user = this.generateRandomUser(locale)
+                newSeed += 10000000
+                faker.seed(newSeed)
+                if (errorCount === 0) {
+                    users.push(user)
+                    continue
+                }
+                const errorUser = this.generateError(user, errorCount)
+                users.push(errorUser)
+            }
+            res.send(users)
         } catch (e) {
             console.log(e)
         }
     }
 
-    generateRandomUser(locale, seed){
+    generateRandomUser(locale) {
 
-        const locales = {
-            en,
-            ru,
-            es
-        }
-
-        const faker = new Faker({locale: locales[locale]})
-        faker.seed(seed)
         return {
-            id: this.generateID(faker, locale),
-            name: this.generateFullName(faker),
-            phone: this.generatePhoneNumber(faker),
-            address: this.generateAddress(faker),
+            id: this.generateID(locale),
+            name: this.generateFullName(),
+            phone: this.generatePhoneNumber(),
+            address: this.generateAddress(),
         }
     }
 
-    generateFullName(f) {
-        return f.person.fullName()
+    generateFullName() {
+        return this.faker.person.fullName()
     }
 
-    generatePhoneNumber(faker) {
+    generatePhoneNumber() {
         const formats = ["human", "international", "national"]
-        const f = formats[this.getRandomInt(faker,formats.length) - 1]
-        return faker.phone.number({style: f})
+        const f = formats[this.getRandomInt(formats.length) - 1]
+        return this.faker.phone.number({style: f})
     }
 
-    getRandomInt(faker, max, min = 1) {
-        return faker.number.int({max, min})
+    getRandomInt(max, min = 1) {
+        return this.faker.number.int({max, min})
     }
 
-    generateAddress(faker) {
+    generateAddress() {
         let address = ""
-        if (this.getRandomInt(faker,2) % 2 === 0) address += `${faker.location.state()}, `
-        address += `${faker.location.city()}, ${faker.location.streetAddress()}`
-        if (this.getRandomInt(faker,2) % 2 === 0) address += ` ${faker.location.secondaryAddress()}`
+        if (this.getRandomInt(this.faker, 2) % 2 === 0) address += `${this.faker.location.state()}, `
+        address += `${this.faker.location.city()}, ${this.faker.location.streetAddress()}`
+        if (this.getRandomInt(this.faker, 2) % 2 === 0) address += ` ${this.faker.location.secondaryAddress()}`
         return address
     }
 
-    generateID(faker, locale) {
+    generateID(locale) {
         const ids = {
             ru: this.generateRuID,
             en: this.generateUsID,
             es: this.generateEsID
         }
-
-        return ids[locale](faker)
+        return ids[locale]()
     }
 
 
-    generateRuID(faker) {
-        return this.getRandomSequence(faker,4) + " " + this.getRandomSequence(faker, 6);
+    generateRuID() {
+        return this.getRandomSequence(4) + " " + this.getRandomSequence(6);
     }
 
-    generateUsID(faker) {
-        return `${faker.string.alpha({length: 2, casing: "upper"})}${this.getRandomSequence(faker, 7)}`
+    generateUsID() {
+        return `${this.faker.string.alpha({length: 2, casing: "upper"})}${this.getRandomSequence(7)}`
     }
 
-    generateEsID(faker) {
-        return `${this.getRandomSequence(faker, 8)}${faker.string.alpha({length: 1, casing: "upper"})}`
+    generateEsID() {
+        return `${this.getRandomSequence(8)}${this.faker.string.alpha({length: 1, casing: "upper"})}`
     }
 
-    getRandomSequence(faker, sequenceLength) {
+    getRandomSequence(sequenceLength) {
         let s = ""
         for (let i = 0; i < sequenceLength; i++) {
-            s += this.getRandomInt(faker, 9, 0)
+            s += this.getRandomInt(9, 0)
         }
         return s;
+    }
+
+    changeIndexError(str) {
+
+        const strArr = str.split("")
+        const to = this.getRandomInt(str.length - 2, 0)
+        let t = strArr[to]
+        let t2 = strArr[to + 1]
+        strArr[to] = t2
+        strArr[to + 1] = t
+        return strArr.join("")
+
+
+    }
+
+    addSymbolError(str) {
+        const strArr = str.split("")
+        const to = this.getRandomInt(str.length - 1, 0)
+        if (this.getRandomInt(1, 0)) {
+            const int = this.getRandomInt(9, 0)
+            let firstPart = strArr.slice(0, to)
+            firstPart.push(int)
+            firstPart = firstPart.concat(...strArr.slice(to))
+            return firstPart.join("")
+
+        } else {
+            let letter = this.faker.string.alpha({length: 1})
+
+            if (this.getRandomInt(1, 0)) {
+                letter = letter.toUpperCase()
+            }
+            let firstPart = strArr.slice(0, to)
+            firstPart.push(letter)
+            firstPart = firstPart.concat(strArr.slice(to))
+            return firstPart.join("")
+        }
+
+
+    }
+
+    deleteSymbolError(str) {
+        const strArr = str.split("")
+        const to = this.getRandomInt(str.length - 1, 0)
+        return strArr.slice(0, to).concat(strArr.slice(to + 1)).join("")
+    }
+
+    oneErrorInData(str, key) {
+        const errorTypes = [this.addSymbolError, this.deleteSymbolError, this.changeIndexError]
+        let temp = String(str)
+
+        let t = this.getRandomInt(errorTypes.length - 1, 0)
+        if (temp.length < 3 && errorTypes[t] !== this.addSymbolError) {
+            return temp
+        }
+        if (temp.length > 20 && errorTypes[t] === this.addSymbolError) {
+            return temp
+        }
+        temp = errorTypes[t](temp)
+        return temp
+
+
+    }
+
+    randomErrorInUser(user, errorCount) {
+        const keys = Object.keys(user)
+        for (let i = 0; i < errorCount; i++) {
+            const randomIndex = this.getRandomInt(keys.length - 1, 0)
+            const newValue = this.oneErrorInData(user[keys[randomIndex]], keys[randomIndex])
+            user[keys[randomIndex]] = newValue
+        }
+        return user
+    }
+
+    generateError(user, errorCount) {
+        const rounded = Math.floor(errorCount)
+        user = this.randomErrorInUser(user, rounded)
+        if (rounded !== errorCount) {
+            const isError = !!this.getRandomInt(1, 0)
+            if (isError) {
+                user = this.randomErrorInUser(user, 1)
+            }
+        }
+        return user
     }
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 5000
+const PORT = 5001
 const router = require("./router")
 
 app.use(express.json());
