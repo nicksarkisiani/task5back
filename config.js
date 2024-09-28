bdf3368d9b6a690cf3b29e5e4540a6f840e4213b
Haster1337 <hasterpro@gmail.com>
2024-09-29 01:34:06 +0400

    cors test

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    client_url: "localhost:5173"
+}
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const PORT = 5001
 const router = require("./router")
+const {client_url} =require("./config")
 
 app.use(express.json());
 
 app.use("/user", router)
+app.user(cors({
+    credentials: true,
+    origin: client_url
+}))
 
 const start = () => {
     app.listen(PORT, () => console.log("server started"));
