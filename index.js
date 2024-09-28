9a2427fa48c94bb1e7714de0d2f1a31e95ee562b
Haster1337 <hasterpro@gmail.com>
2024-09-29 02:22:30 +0400

    cors test

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const {client_url} =require("./config")
 
 app.use(express.json());
 
-app.use("/user", router)
 app.use(cors({
-    origin: 'http://localhost:5173', // Разрешить запросы с твоего фронта
-    credentials: true, // Если используешь куки или авторизацию
+    origin: '*',
+    credentials: true,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization"
 }));
-app.options('*', cors());
+
+app.use("/user", router)
+
 
 const start = () => {
     app.listen(PORT, () => console.log("server started"));
