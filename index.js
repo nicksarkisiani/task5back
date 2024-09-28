ae50a40bd1bd9ba39e53982ddd74f80ceca29e89
Haster1337 <hasterpro@gmail.com>
2024-09-29 02:07:33 +0400

    cors test

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ app.use(express.json());
 app.use("/user", router)
 app.use(cors({
     credentials: true,
-    origin: function (origin, callback) {
-        callback(null, true);
-    },
+    origin: "*"
 }))
 
 const start = () => {
